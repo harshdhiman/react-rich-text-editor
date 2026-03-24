5a38c2783de4622dd92df48e5705af66b514ede7
harshdhiman <harshdhiman54@gmail.com>
2026-03-24 17:50:54 +0530

    cleanup

--- src/RichTextEditor.tsx
+++ src/RichTextEditor.tsx
@@ -1296,7 +1296,6 @@ const RichTextEditor = forwardRef<RichTextEditorRef, RichTextEditorProps>(
       (mediaAttachment: MediaAttachment) => {
         const editor = editorRef.current;
         if (!editor || !mediaAttachment.uri) return;
-        alert("asdqwdqwd22");
         editor.focus();
 
         const mediaDiv = createMediaAttachmentElement(mediaAttachment);
